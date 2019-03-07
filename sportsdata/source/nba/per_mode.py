0d90557dc1e83c8b6b58b35be471e8d1582542c9
OrangeCardinal <david.scott.orkin@gmail.com>
2019-03-07 13:36:58 -0700

    Convenience Enums for the standard parameters.

---
+++ sportsdata/source/nba/per_mode.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class PerMode(Enum):
+    TOTALS      =   "Totals"
+    PER_GAME    =   "PerGame"
+    PER_36      =   "Per36"
---
+++ sportsdata/source/nba/season_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class SeasonType(Enum):
+    ALL_STAR        =   "All-Star"
+    REGULAR_SEASON  =   "Regular Season"
+    PRE_SEASON      =   "Pre Season"
+    PLAYOFFS        =   "Playoffs"
