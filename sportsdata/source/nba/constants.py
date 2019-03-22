af7a0bc36b0dfdfa18352183bcbce3879db060af
OrangeCardinal <david.scott.orkin@gmail.com>
2019-03-21 21:21:22 -0600

    Enum classes are now in constants. team.py is deprecated by the teams/ directory code.

--- sportsdata/source/nba/constants.py
+++ sportsdata/source/nba/constants.py
@@ -1,5 +1,12 @@
 from enum import Enum
 
+
+class PerMode(Enum):
+    TOTALS      =   "Totals"
+    PER_GAME    =   "PerGame"
+    PER_36      =   "Per36"
+
+
 class SeasonType(Enum):
     ALL_STAR        =   "All-Star"
     REGULAR_SEASON  =   "Regular Season"
--- sportsdata/source/nba/per_mode.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class PerMode(Enum):
-    TOTALS      =   "Totals"
-    PER_GAME    =   "PerGame"
-    PER_36      =   "Per36"
