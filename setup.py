f6248542db7e7ab8ee69bbf85b6c910955de59cf
OrangeCardinal <david.scott.orkin@gmail.com>
2019-03-27 20:37:14 -0600

    Updated to reflect the name change from sportsdata to sports.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sportsdata",
-    version="0.0.1",
+    name="sports",
+    version="0.0.0",
     author="David Scott Orkin",
     author_email="orange.cardinal.technology@gmail.com",
     description="APIs for gathering data from professional sport websites",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/OrangeCardinal/sportsdata",
+    url="https://github.com/OrangeCardinal/sports",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
